aa98c37662d4c45c47dd05c04e4a097dee8876e4
emzhofb <ikhdamuhammad@gmail.com>
2019-02-21 22:01:56 +0700

    props class

--- props/src/components/Table.js
+++ props/src/components/Table.js
@@ -1,24 +1,27 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const Table = (props) => {
-    return (
-        <table>
-          <thead>
-            <th>Nama</th>
-          </thead>
-          <tbody>
-            {
-              props.mugiwara.map((data, index) => {
-                return (
-                  <tr key={index}>
-                    <td>{data.nama}</td>
-                  </tr>
-                )
-              })
-            }
-          </tbody>
-        </table>
-    )
+class Table extends Component {
+    render () {
+        console.log(this.props)
+        return (
+            <table>
+              <thead>
+                <th>Nama</th>
+              </thead>
+              <tbody>
+                {
+                  this.props.mugiwara.map((data, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{data.nama}</td>
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </table>
+        )
+    }
 }
 
 export default Table
